4db74945f81e2f1263bbcee406eda400f951129d
locochon01 <beneplemx2@gmail.com>
2022-12-06 10:47:11 +0900

    Second Commit

--- src/edu/kh/test/testProject/HelloWorld.java
+++ src/edu/kh/test/testProject/HelloWorld.java
@@ -13,6 +13,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		// 실행구문 (메인메소드가 있어야 프로젝트 실행 가능) 
+		
 		System.out.println("HelloWorld~!"); //영어
 		System.out.println("아침은 드셨나요?"); //한글
 		System.out.println("123456"); //숫자
@@ -26,7 +27,20 @@ public class HelloWorld {
 		// " " 안에 작성되지 않은 코드는 숫자, 변수로 인식
 		
 		// "" (문자열) + 숫자 함께 작성
+		
 		System.out.println("14*19=" + 266);
+		// 숫자와 숫자끼리의 덧셈은 
+		// 실제 덧셈 연산결과.
+		// 문자열과 숫자, 혹은 문자열과 문자열의 덧셈은 
+		// ==> 이어쓰기
+		
+		System.out.println ("14 * 19=" + 14 * 19);
+		
+		// notice!
+		// Java는 사칙연산의 우선순위를 그대로 따른다.
+		// ex) 우선계산을 원하면 () 괄호 사용
+		
+		System.out.println ("안녕")
 	}
 
 }
