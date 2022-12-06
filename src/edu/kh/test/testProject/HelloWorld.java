a5d8155e1711e7d5e2b89105458bead506549a0f
DESKTOP-71MQ1C7\user1 <beneplemx2@gmail.com>
2022-12-06 10:40:25 +0900

    first commit

---
+++ src/edu/kh/test/testProject/HelloWorld.java
@@ -0,0 +1,32 @@
+package edu.kh.test.testProject;
+		// package (관련된 코드들의 묶음) 명명 규칙
+		// 1.최소 3레벨 이상
+		// 2. 프로젝트 주소의 기반 (거꾸로)
+
+// : 한 줄 주석
+/*// : 범위주석
+ * 
+ * 주석안에 쓰여진 코드는 컴파일러가 해석하지 않음
+ **/
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// 실행구문 (메인메소드가 있어야 프로젝트 실행 가능) 
+		System.out.println("HelloWorld~!"); //영어
+		System.out.println("아침은 드셨나요?"); //한글
+		System.out.println("123456"); //숫자
+		
+		//" " (쌍따옴표) : 문자열을 나타냄
+		//==> 안에 작성된 텍스트 (코드)는 단순 문자열로 인식
+		
+		System.out.println ("1+2");
+		System.out.println (1+2);
+		
+		// " " 안에 작성되지 않은 코드는 숫자, 변수로 인식
+		
+		// "" (문자열) + 숫자 함께 작성
+		System.out.println("14*19=" + 266);
+	}
+
+}
